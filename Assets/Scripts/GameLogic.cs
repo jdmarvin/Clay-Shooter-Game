c0884a1059cc090ef10078c2adcc0c5e81c2e189
Jason Marvin <jdmarvin2002@yahoo.com>
2017-04-27 16:50:14 -0600

    Clay Pigeon Logic
    Created the logic to shoot a clay pigeon

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour {
+
+	public GameObject clayPigeon;
+	public Transform shootTable;
+
+	void OnCollisionEnter (Collider other) {
+		if (other.gameObject.tag == "Ground") {
+			Destroy (gameObject);
+		}
+	}
+
+	void Update () {
+		if (Input.anyKeyDown) {
+			Instantiate (clayPigeon, shootTable.position, shootTable.rotation);
+		}
+	}
+}
---
+++ Assets/Scripts/ShootClayPigeon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootClayPigeon : MonoBehaviour {
+
+
+	public float clayPigeonThrust;
+
+	private Rigidbody clayPigeonRigidbody;
+
+
+	void Awake () {
+		clayPigeonRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void Start () {
+		clayPigeonRigidbody.AddForce (transform.forward * clayPigeonThrust);
+		clayPigeonRigidbody.useGravity = true;
+
+		//Vector3 direction = clayPigeon.transform.position - new Vector3 (transform.position.x * transform.localScale.x, transform.position.y, transform.position.z);
+		//clayPigeonRigidbody.AddForceAtPosition(direction.normalized, new Vector3 (10f,2f,10f));
+	}
+
+
+}
