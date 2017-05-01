7313732eb6d2f5012125a6217f8784cb3963ba75
Jason Marvin <jdmarvin2002@yahoo.com>
2017-05-01 13:12:42 -0600

    Update Unity, destroy gameobjects
    Updated Unity to 5.6 and destroyed clay pigeon game objects using
    lifetime

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -7,12 +7,6 @@ public class GameLogic : MonoBehaviour {
 	public GameObject clayPigeon;
 	public Transform shootTable;
 
-	void OnCollisionEnter (Collider other) {
-		if (other.gameObject.tag == "Ground") {
-			Destroy (gameObject);
-		}
-	}
-
 	void Update () {
 		if (Input.anyKeyDown) {
 			Instantiate (clayPigeon, shootTable.position, shootTable.rotation);
