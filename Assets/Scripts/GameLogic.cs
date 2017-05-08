e81f29d91ac6645cc94e24bfd58d800b18bfc996
Jason Marvin <jdmarvin2002@yahoo.com>
2017-05-08 15:10:13 -0600

    Clay Wave
    Wrote a clay pigeon wave program

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -7,9 +7,34 @@ public class GameLogic : MonoBehaviour {
 	public GameObject clayPigeon;
 	public Transform shootTable;
 
+	public float waveStart;
+	public float waveDelay;
+
+	public bool clayWave;
+
+		
 	void Update () {
 		if (Input.GetKeyDown("space")) {
 			Instantiate (clayPigeon, shootTable.position, shootTable.rotation);
 		}
+	}
+
+	void StartClayWave () {
+
+		if (clayWave) {
+
+			StartCoroutine (ClayWaves ());
+		}
+	}
+
+	IEnumerator ClayWaves () {
+
+		yield return new WaitForSeconds (waveStart);
+		while (true) {
+
+			Instantiate (clayPigeon, shootTable.position, shootTable.rotation);
+			yield return new WaitForSeconds (waveDelay);
+		}
+
 	}
 }
