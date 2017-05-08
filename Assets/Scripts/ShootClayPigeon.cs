22473bdd1394359fc5fe2174cd058f8778e4e9c2
Jason Marvin <jdmarvin2002@yahoo.com>
2017-05-08 12:45:51 -0600

    Explosion
    Created Explosion and Instantiated it when clay pigeon is shot.

--- Assets/Scripts/ShootClayPigeon.cs
+++ Assets/Scripts/ShootClayPigeon.cs
@@ -5,32 +5,13 @@ using UnityEngine.EventSystems;
 
 public class ShootClayPigeon : MonoBehaviour {
 
-	public float force;
-
-	private Rigidbody rb;
-
-
-	void Start () {
-	
-		rb = GetComponent<Rigidbody> ();
-	}
-
-	void Update() {
-
-		if (Input.GetMouseButtonDown(0)) {
-
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;
-
-			if (Physics.Raycast (ray, out hit, 100)) {
-
-				rb.AddExplosionForce (force, hit.point, 5, 0, ForceMode.Impulse);
-			}
-		}
-	}
+	public GameObject clayPigeonExplosion;
 
 
 	void OnMouseDown() {
+		
+		Destroy (gameObject);
+		Instantiate (clayPigeonExplosion, transform.position, transform.rotation);
 		Debug.Log("You have hit it!");
 	}
 		
